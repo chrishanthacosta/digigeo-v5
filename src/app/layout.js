056a787ae0a8befa7487e10a6f3fd44b2ace7113
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2024-08-08 10:08:25 +0530

    refactor: Add Google Analytics tracking code to HTML head

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import LayoutClientComponent from "./layout-client";
 import HomeNavbar from "./components/nav-bar/navbar-component";
- 
+import Head from "next/head";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -16,7 +17,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-MWTV2JLXR0"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-MWTV2JLXR0');
+            `,
+          }}
+        />
+      </Head>
       <body className={`${inter.className} overflow-hidden h-screen`}>
         {/* <body className={inter.className}> */}
         <LayoutClientComponent>
